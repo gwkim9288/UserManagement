afbd711a3182c4b0d35e31b9eac87136ca12a697
김근우 <slalek152@naver.com>
2021-03-20 23:55:15 +0900

    /user validation check, new commit in hateoas

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.UserEntity;
 import com.example.demo.exception.UserNotFoundException;
 import com.example.demo.resource.UserResourceAssembler;
 import com.example.demo.service.UserService;
+import com.example.demo.validation.UserValidator;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
 import org.hibernate.EntityMode;
@@ -12,21 +13,24 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.validation.*;
 
 import javax.swing.text.html.parser.Entity;
 import java.net.URI;
 import java.util.List;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(value="/users")
 public class UserController{
 
     private final UserService userService;
     private final UserValidator userValidator;
 
-    @GetMapping("/users/{user_id}/get")
+    @GetMapping("{user_id}")
     public ResponseEntity getUserByID(@PathVariable int user_id){
         UserEntity user = userService.getUserById(user_id);
         if(user==null){
@@ -35,17 +39,19 @@ public class UserController{
         //Hateaos 적용하여 resource받아오기
         UserResourceAssembler userResourceAssembler = new UserResourceAssembler();
         EntityModel<UserEntity> userResource = userResourceAssembler.toModel(user);
-        userResource.add(linkTo(UserController.class).withRel("getUserByID"));
 
-        return ResponseEntity.ok(userResourceAssembler.toModel(user));
+        //add list uri link
+        userResource.add(linkTo(methodOn(UserController.class).getList()).withRel("getList"));
+
+        return ResponseEntity.ok(userResource);
     }
 
-    @GetMapping("/users/list")
+    @GetMapping("/list")
     public List<UserEntity> getList(){
         return userService.getList();
     }
 
-    @PostMapping("/users/post")
+    @PostMapping("/")
     public ResponseEntity<UserEntity> createUser(@RequestBody @Valid UserEntity user){
         userService.createUser(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -53,12 +59,12 @@ public class UserController{
         return ResponseEntity.created(location).build();
     }
 
-    @DeleteMapping("/users/{user_id}/delete")
+    @DeleteMapping("/{user_id}")
     public boolean deleteUser(@PathVariable int user_id){
         return userService.deleteUserById(user_id);
     }
 
-    @PostMapping("/users/{user_id}/updata")
+    @PostMapping("/{user_id}")
     public void updateUser(@RequestBody UserEntity user, @PathVariable int user_id){
         if(userService.updateUser(user_id,user)) {
             throw new UserNotFoundException(String.format("User Not Found : id = %d", user_id));
--- src/main/java/com/example/demo/validation/UserValidatior.java
+++ src/main/java/com/example/demo/validation/UserValidatior.java
@@ -1,4 +1,30 @@
 package com.example.demo.validation;
 
-public class UserValidatior {
+import com.example.demo.domain.UserDTO;
+import com.example.demo.domain.UserEntity;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+@RequiredArgsConstructor
+public class UserValidator implements Validator {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return UserDTO.class.isAssignableFrom(aClass);
+    }
+
+    public void validate(Object target, Errors errors) {
+        UserDTO userDTO = (UserDTO) target;
+        UserEntity user1 =  userRepository.findById(userDTO.getName());
+        if (user1 != null) {
+            errors.rejectValue("username", "wrongValue", "someone already use this name");
+        }
+    }
 }
